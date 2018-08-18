65b2ccd7e84d414985ae842c62e388567c8a2c10
cdaringe <christopher.dieringer@gmail.com>
2018-08-17 20:14:27 -0700

    init

---
+++ readme.md
@@ -0,0 +1,8 @@
+# standardjs complians on typescript interfaces
+
+## reproduce
+
+- `yarn`
+- `yarn lint`
+
+observe source in `src/index.js`
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+export interface Obama {
+  phone: number
+  name: string
+}
+const myMainMan: Obama = {
+  phone: 8675309,
+  name: 'barack obama'
+}
+console.log(myMainMan)
